18d3471643d8233c7e711ff220d1a71f67589093
Hakan Uçar <hakanucaar@yandex.com>
2020-08-24 00:35:36 +0300

    Updates

---
+++ precache-manifest.7b544e2e35e7820bea88e6b77d788c0f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec303917f852c6f96d3ecb0371fdddc9",
+    "url": "/TrendyolCollectionShare/index.html"
+  },
+  {
+    "revision": "07f7be175c9b9f93bda6",
+    "url": "/TrendyolCollectionShare/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "75244c332b7e469a1607",
+    "url": "/TrendyolCollectionShare/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "07f7be175c9b9f93bda6",
+    "url": "/TrendyolCollectionShare/static/js/2.a80a34e7.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/TrendyolCollectionShare/static/js/2.a80a34e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75244c332b7e469a1607",
+    "url": "/TrendyolCollectionShare/static/js/main.a6df8bf8.chunk.js"
+  },
+  {
+    "revision": "3ec845d56895cc689e0a",
+    "url": "/TrendyolCollectionShare/static/js/runtime-main.ad78fe7d.js"
+  },
+  {
+    "revision": "f096091909e5f7a5c0d86d4556ec9622",
+    "url": "/TrendyolCollectionShare/static/media/Img2.f0960919.jpg"
+  },
+  {
+    "revision": "f653dce9229987f5b1121081d3288f01",
+    "url": "/TrendyolCollectionShare/static/media/summer.f653dce9.jpg"
+  }
+]);
